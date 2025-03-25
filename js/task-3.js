54361f8386e9092333dfb539611cfdcca5884b84
berkangormus <berkangormus@outlook.com>
2025-03-25 22:41:30 +0300

    end

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+class StringBuilder
+{
+    #value;
+    constructor(starterValue)
+    { this.#value = starterValue; }
+    getValue()
+    { return this.#value; }
+    padStart(string)
+    { this.#value = `${string}${this.#value}`; }
+    padEnd(string)
+    { this.#value = `${this.#value}${string}`; }
+    padBoth(string)
+    { this.padStart(string);
+      this.padEnd(string); }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue());  // "."
+builder.padStart("^");
+console.log(builder.getValue());  // "^."
+builder.padEnd("^");
+console.log(builder.getValue());  // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue());  // "=^.^="
